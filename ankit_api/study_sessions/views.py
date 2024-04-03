dbc50991a6af1ee6d5435f38136ddcfcdae33d20
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-04-03 17:47:13 -0300

    StudySession's ViewSet and Serializer implemented.

--- ankit_api/study_sessions/views.py
+++ ankit_api/study_sessions/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from .models import StudySession
+from rest_framework import permissions, viewsets
+
+from .serializers import StudySessionSerializer
+
+
+class StudySessionViewSet(viewsets.ModelViewSet):
+    queryset = StudySession.objects.all()
+    serializer_class = StudySessionSerializer
+    permission_classes = [permissions.IsAuthenticated]
