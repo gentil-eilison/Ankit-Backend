349ae42bcac773e6cde9c828fe974acbbd814cba
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2024-04-02 22:13:15 -0300

    feature: creates all models based on logic diagram

--- ankit_api/study_sessions/models.py
+++ ankit_api/study_sessions/models.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from simple_history import models as simple_history_models
+
+from ankit_api.core.models import TimeStampedModel
+from ankit_api.users.utils import user_directory_path
 
 
 class Language(models.Model):
@@ -14,3 +18,34 @@ class Language(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class StudySession(TimeStampedModel):
+    duration_in_minutes = models.DurationField(verbose_name=_("Duration in minutes"))
+    cards_added = models.PositiveSmallIntegerField(verbose_name=_("Cards added"))
+    spreadsheet_file = models.FileField(
+        upload_to=user_directory_path,
+        verbose_name=_("Spreadsheet file"),
+    )
+    language = models.ForeignKey(
+        Language,
+        related_name="study_sessions",
+        on_delete=models.CASCADE,
+        verbose_name=_("Language"),
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="study_sessions",
+        on_delete=models.CASCADE,
+        verbose_name=_("User"),
+    )
+    history = simple_history_models.HistoricalRecords()
+
+    class Meta:
+        verbose_name = _("Study Session")
+        verbose_name_plural = _("Study Sessions")
+
+    def __str__(self):
+        return (
+            f"{self.language} session of {self.user.first_name} {self.user.last_name}"
+        )
--- ankit_api/users/models.py
+++ ankit_api/users/models.py
@@ -1,13 +1,26 @@
-
 from typing import ClassVar
 
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-from django.db.models import EmailField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from simple_history import models as simple_history_models
+
+from ankit_api.core.models import TimeStampedModel
 
 from .managers import UserManager
+
+
+class Nationality(TimeStampedModel):
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
+    history = simple_history_models.HistoricalRecords()
+
+    class Meta:
+        verbose_name = _("Nationality")
+        verbose_name_plural = _("Nationalities")
+
+    def __str__(self):
+        return self.name
 
 
 class User(AbstractUser):
@@ -17,12 +30,34 @@ class User(AbstractUser):
     check forms.SignupForm and forms.SocialSignupForms accordingly.
     """
 
+    class EducationalLevels(models.TextChoices):
+        MIDDLE_SCHOOL = "Middle School", _("Ensino Fundamental")
+        HIGH_SCHOOL = "High School", _("Ensino Médio")
+        UNIVERSITY = "University", _("Superior")
+
     # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore[assignment]
-    last_name = None  # type: ignore[assignment]
-    email = EmailField(_("email address"), unique=True)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    first_name = models.CharField(max_length=255, verbose_name=_("First name"))
+    last_name = models.CharField(max_length=255, verbose_name=_("Last name"))
+    email = models.EmailField(_("email address"), unique=True)
     username = None  # type: ignore[assignment]
+    educational_level = models.CharField(
+        max_length=13,
+        choices=EducationalLevels.choices,
+        verbose_name=_("Educational level"),
+    )
+    streak = models.PositiveSmallIntegerField(verbose_name=_("Streak"), default=0)
+    longest_streak = models.PositiveSmallIntegerField(
+        verbose_name=_("Longest streak"),
+        default=0,
+    )
+    nationality = models.ForeignKey(
+        Nationality,
+        related_name="users",
+        on_delete=models.PROTECT,
+        verbose_name=_("Nationality"),
+    )
+    history = simple_history_models.HistoricalRecords()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -30,10 +65,4 @@ class User(AbstractUser):
     objects: ClassVar[UserManager] = UserManager()
 
     def get_absolute_url(self) -> str:
-        """Get URL for user's detail view.
-
-        Returns:
-            str: URL for user detail.
-
-        """
         return reverse("users:detail", kwargs={"pk": self.id})
