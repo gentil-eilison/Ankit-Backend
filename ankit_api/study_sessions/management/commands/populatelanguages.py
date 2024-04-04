80ccb6a31f8dc426defeba21b0fdfec7f0d75e2e
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-04-03 21:53:37 -0300

    Languages population manage command implemented.

---
+++ ankit_api/study_sessions/management/commands/populatelanguages.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+
+from ankit_api.study_sessions.models import Language
+
+
+class Command(BaseCommand):
+    help = "Creates a new language"
+
+    def handle(self, *args, **options) -> str | None:
+        languages = (
+            "Inglês",
+            "Francês",
+            "Espanhol",
+            "Alemão",
+            "Italiano",
+            "Dinamarquês",
+            "Finlandês",
+            "Ucraniano",
+            "Russo",
+            "Chinês (Mandarim)",
+            "Coreano",
+            "Japonês",
+            "Sueco",
+        )
+
+        for language in languages:
+            Language.objects.get_or_create(name=language)
+
+        self.stdout.write(
+            self.style.SUCCESS("Languages successfully populated into the database."),
+        )
