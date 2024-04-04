13b5294340b9b5a103bc9c4c6a9600c28d093321
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2024-04-03 22:17:59 -0300

    feature(study_session): starts VocabularyBuilderView development

--- ankit_api/study_sessions/views.py
+++ ankit_api/study_sessions/views.py
@@ -1,8 +1,26 @@
-# Create your views here.
-from .models import StudySession
-from rest_framework import permissions, viewsets
+from rest_framework import permissions
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from .models import StudySession
 from .serializers import StudySessionSerializer
+from .serializers import VocabularyBuilderSerializer
+
+
+class VocabularyBuilderView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        vocabulary_builder_serializer = VocabularyBuilderSerializer(data=request.POST)
+        if vocabulary_builder_serializer.is_valid():
+            # TODO: make call to chat GPT API using facade class
+            pass
+        return Response(
+            status=status.HTTP_400_BAD_REQUEST,
+            data=vocabulary_builder_serializer.errors,
+        )
 
 
 class StudySessionViewSet(viewsets.ModelViewSet):
