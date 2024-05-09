3eeb580bb7ec20f9ec90a36f17dda5d9cbd52534
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-05-09 09:23:00 -0300

    feature: Django's ORM now already gets all students who didn't study that day.

--- ankit_api/users/tasks.py
+++ ankit_api/users/tasks.py
@@ -6,17 +6,12 @@ from .models import User
 
 @shared_task()
 def validate_streak():
-    all_users = User.objects.all()
-    for user_to_validate_streak in all_users:
+    users_who_didnt_study = User.objects.filter(student__studied_today=False)
+    for user_to_validate_streak in users_who_didnt_study:
         try:
             student = user_to_validate_streak.student
-
-            if not student.studied_today:
-                student.longest_streak = student.streak
-                student.streak = 0
-                student.save()
-            else:
-                student.streak += 1
-                student.save()
+            student.longest_streak = student.streak
+            student.streak = 0
+            student.save()
         except Student.DoesNotExist:
             continue
