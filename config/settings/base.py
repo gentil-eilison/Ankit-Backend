06f747b60526b89bc9d0312fe9f0137a1589d0ad
Gentil Ályson Jácome Fernandes <fernandes.gentilalysonjac@gmail.com>
2024-08-15 21:24:17 -0300

    fix: sets history_user to sentinel user when deleting study sessions

--- config/settings/base.py
+++ config/settings/base.py
@@ -359,7 +359,7 @@ SPECTACULAR_SETTINGS = {
     "TITLE": "ankit_api API",
     "DESCRIPTION": "Documentation of API endpoints of ankit_api",
     "VERSION": "1.0.0",
-    "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
+    "SERVE_PERMISSIONS": ["rest_framework.permissions.AllowAny"],
 }
 # Your stuff...
 # ------------------------------------------------------------------------------
